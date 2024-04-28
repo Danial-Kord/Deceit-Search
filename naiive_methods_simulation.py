35a20be95f3687c3f3cf6c8dd2dca2a7e2450050
Danial-Kord <danial.kordmodanlou@gmail.com>
2024-04-28 15:46:24 -0400

    iive base simulation for both continues and discrete case

--- naiive_methods_simulation.py
+++ naiive_methods_simulation.py
@@ -1,88 +1,10 @@
 import random
 import csv
 
-#Keep asking each quesrtion for 2*E+1 times
-def naiive_approach(number_of_lies = 4):
-    target = tree.target
-    value = -1
-    node = tree.root
-    print("Target: ", target)
-    number_of_questions = 0
-    while value != target:
-        number_of_questions += 1
-        print("Current node: ", node.value)
-        # random direction
-        direction = random.choice([True, False])  # True is right, False is left
-        answers = 0
-        print("Is the target to the {}?".format("right" if direction else "left"))
-        for i in range(2 * number_of_lies + 1):
-            number_of_questions += 1
-            answer = tree.direction(node, direction)
-            print("Answer: ", answer)
-            answers += 1 if answer else -1
-        go_right = False
-        if answers >= 0:
-            go_right = direction
-        else:
-            go_right = not direction
-        if go_right:
-            print("Go right")
-            node = node.right
-        else:
-            print("Go left")
-            node = node.left
-        value = node.value
-
-    print("Target found!", value)
-    return number_of_questions
-
-# Keep tracking the number of questions asked
-def historical_naiive_approach(E = 4):
-    target = tree.target
-    value = -1
-    node = tree.root
-    print("Target: ", target)
-    number_of_questions = 0
-    total_received_lies = 0
-    number_of_lies = E
-    while value != target:
-        number_of_questions += 1
-        print("Current node: ", node.value)
-        # random direction
-        direction = random.choice([True, False])  # True is right, False is left
-        answers = 0
-        answers_list = []
-        print("Is the target to the {}?".format("right" if direction else "left"))
-        for i in range(2 * number_of_lies + 1):
-            number_of_questions += 1
-            answer = tree.direction(node, direction)
-            answers_list.append(answer)
-            print("Answer: ", answer)
-            answers += 1 if answer else -1
-        number_of_lies_in_answers = min(answers_list.count(True), answers_list.count(False))
-        total_received_lies += number_of_lies_in_answers
-        number_of_lies = E - total_received_lies
-        go_right = False
-        if answers >= 0:
-            go_right = direction
-        else:
-            go_right = not direction
-        if go_right:
-            print("Go right")
-            node = node.right
-        else:
-            print("Go left")
-            node = node.left
-        value = node.value
-
-    print("Target found!", value)
-    return number_of_questions
-
-
 
 import numpy as np
 
-def experiment(size, total_number_of_lies):
+def experiment(size, total_number_of_lies, probability = 0.5, arbitrary=False):
     number_of_lies = total_number_of_lies
     number_of_seen_lies = 0
     num_asked_questions = 0
@@ -91,7 +13,7 @@ def experiment(size, total_number_of_lies):
 
     # Random selection using NumPy
     target = np.random.choice(size)
-    lie_places = np.random.choice(size.bit_length() * (number_of_lies * 2 + 1), number_of_lies, replace=False)
+    # lie_places = np.random.choice(size.bit_length() * (number_of_lies * 2 + 1), number_of_lies, replace=False)
 
 
 
@@ -107,16 +29,16 @@ def experiment(size, total_number_of_lies):
         i = 0
         new_lie_detected = False
         while i < 2 * number_of_lies + 1:
-            if num_asked_questions in lie_places:
+            if number_of_lies != 0 and random.random() < probability:
                 number_of_seen_lies += 1
                 new_lie_detected = True
             i += 1
             num_asked_questions += 1
 
         number_of_lies = total_number_of_lies - number_of_seen_lies
-        if new_lie_detected:
-            # Update lie_places range for subsequent rounds
-            lie_places = np.random.choice(np.arange(num_asked_questions, num_asked_questions + (max_num - min_num).bit_length() * ((total_number_of_lies - number_of_seen_lies) * 2 + 1)), total_number_of_lies - number_of_seen_lies, replace=False)
+        # if new_lie_detected:
+        #     # Update lie_places range for subsequent rounds
+        #     lie_places = np.random.choice(np.arange(num_asked_questions, num_asked_questions + (max_num - min_num).bit_length() * ((total_number_of_lies - number_of_seen_lies) * 2 + 1)), total_number_of_lies - number_of_seen_lies, replace=False)
 
     if number_of_seen_lies < total_number_of_lies:
         num_asked_questions += (total_number_of_lies - number_of_seen_lies) * 2 + 1
@@ -125,35 +47,119 @@ def experiment(size, total_number_of_lies):
     return num_asked_questions
 
 
-# Prepare CSV file
-with open('experiment_results.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Experiment Number", "Size", "Number of Lies", "Mean Questions", "Std Deviation"])
+def continues_case(epsilon, probability = 0.5):
+    total_number_of_lies = int((1 / epsilon) * probability)
+    number_of_lies = total_number_of_lies
+
+    number_of_seen_lies = 0
+    num_asked_questions = 0
 
 
-    # Sample usage
-    sizes = [100, 1000, 10000, 100000, 1000000, 10000000, 100000000]
-    lie_fraction = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+    # Random float between 0 and 1
+    target = random.random()
+
+    # lie_places = np.random.choice(size.bit_length() * (number_of_lies * 2 + 1), number_of_lies, replace=False)
+
+    max_num = 1.0
+    min_num = 0.0
+    # print("Target: ", target)
+    while max_num - min_num > epsilon:
+
+        guess = (max_num + min_num) / 2.0
+
+        is_it_right = np.random.choice([True, False])  # Randomly determine direction
+
+        if guess <= target:
+            min_num = guess
+        else:
+            max_num = guess
+
+        i = 0
+        new_lie_detected = False
+        while i < 2 * number_of_lies + 1:
+            if number_of_lies != 0 and random.random() < probability:
+                number_of_seen_lies += 1
+                new_lie_detected = True
+            i += 1
+            num_asked_questions += 1
+
+        number_of_lies = total_number_of_lies - number_of_seen_lies
+        # if new_lie_detected:
+        #     # Update lie_places range for subsequent rounds
+        #     lie_places = np.random.choice(np.arange(num_asked_questions, num_asked_questions + (max_num - min_num).bit_length() * ((total_number_of_lies - number_of_seen_lies) * 2 + 1)), total_number_of_lies - number_of_seen_lies, replace=False)
+
+    if number_of_seen_lies < total_number_of_lies:
+        num_asked_questions += (total_number_of_lies - number_of_seen_lies) * 2 + 1
+
+    # print("Number of questions asked: ", num_asked_questions)
+    # print("Target: ", target)
+    # print("max_num: ", max_num)
+    # print("min_num: ", min_num)
+    # print(index)
+    return num_asked_questions
 
 
 
-    experiment_number = 0
-    max_itr_number = 1000
-    for size in sizes:
-        for fraction in lie_fraction:
-            experiment_number += 1
-            experimentResults = []
-            print("-" * 50)
-            print("Experiment number: ", experiment_number)
-            print("Number of lies: ", int(size * fraction))
-            print("Size: ", size)
-            for i in range(max_itr_number):
-                # print("Size: ", size, "Fraction: ", fraction)
-                experimentResults.append(experiment(size, int(size * fraction)))
-            print("Results: ", experimentResults)
-            print("mean and std: ", np.mean(experimentResults), np.std(experimentResults))
-            writer.writerow([experiment_number, size, int(size * fraction), np.mean(experimentResults), np.std(experimentResults)])
-            print("-" * 50)
+def simulate_naiive_historical():
+    # Prepare CSV file
+    with open('experiment_results.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Experiment Number", "Size", "Number of Lies", "Mean Questions", "Std Deviation"])
+
+        # Sample usage
+        sizes = [100, 1000, 10000, 100000, 1000000, 10000000, 100000000]
+        lie_fraction = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+
+        experiment_number = 0
+        max_itr_number = 1000
+        for size in sizes:
+            for fraction in lie_fraction:
+                experiment_number += 1
+                experimentResults = []
+                print("-" * 50)
+                print("Experiment number: ", experiment_number)
+                print("Number of lies: ", int(size * fraction))
+                print("Size: ", size)
+                for i in range(max_itr_number):
+                    # print("Size: ", size, "Fraction: ", fraction)
+                    experimentResults.append(experiment(size, int(size * fraction), probability=fraction))
+                print("Results: ", experimentResults)
+                print("mean and std: ", np.mean(experimentResults), np.std(experimentResults))
+                writer.writerow([experiment_number, size, int(size * fraction), np.mean(experimentResults),
+                                 np.std(experimentResults)])
+                print("-" * 50)
+
+def simulate_naiive_historical_continues_case():
+    # Prepare CSV file
+    with open('cs_case_experiment_results.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Experiment Number", "Epsilon", "Number of Lies", "Mean Questions", "Std Deviation"])
+
+        # Sample usage
+        epsilons = [0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001, 0.000000001]
+        lie_fraction = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+
+        experiment_number = 0
+        max_itr_number = 1000
+        for epsilon in epsilons:
+            for fraction in lie_fraction:
+                experiment_number += 1
+                experimentResults = []
+                print("-" * 50)
+                print("Experiment number: ", experiment_number)
+                print("Number of lies: ", int((1.0/epsilon) * fraction))
+                print("Epsilon: ", epsilon)
+                for i in range(max_itr_number):
+                    # print("Size: ", size, "Fraction: ", fraction)
+                    experimentResults.append(continues_case(epsilon, probability=fraction))
+                print("Results: ", experimentResults)
+                print("mean and std: ", np.mean(experimentResults), np.std(experimentResults))
+                writer.writerow([experiment_number, epsilon, int((1.0/epsilon) * fraction), np.mean(experimentResults),
+                                 np.std(experimentResults)])
+                print("-" * 50)
+
+
+simulate_naiive_historical_continues_case()
 
 # experiment(10000, 50)
 
